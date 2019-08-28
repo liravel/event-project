0141243d56e2e16dd2777c2b264e8df78a6813bd
Viktoryia Valiuk <ackermanviktoria@gmail.com>
2019-08-28 02:09:22 +0200

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,9 @@
+var changeZIndex = function(id, current) {
+  var currentId = [...current].slice(0)[0].id;
+  var currentNode = document.getElementById(currentId);
+  var selectedNode = document.getElementById(id);
+  currentNode.classList.remove("top");
+  currentNode.style.zIndex = "0";
+  selectedNode.style.zIndex = "1";
+  selectedNode.classList.add("top");
+}
