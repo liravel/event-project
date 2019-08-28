32f8dd90ff54d3e46ac5f140c9f52198966f6a41
Viktoryia Valiuk <ackermanviktoria@gmail.com>
2019-08-28 05:51:00 +0200

    toggle display value

--- js/main.js
+++ js/main.js
@@ -4,6 +4,8 @@ var changeZIndex = function(id, current) {
   var selectedNode = document.getElementById(id);
   currentNode.classList.remove("top");
   currentNode.style.zIndex = "0";
+  currentNode.style.display = "none"
   selectedNode.style.zIndex = "1";
+  selectedNode.style.display = "flex";
   selectedNode.classList.add("top");
 }
