42ee2e2e3ba29b50a59564759dd090c34eb7bcf1
tkozoh311 <52952418+tkozoh311@users.noreply.github.com>
2019-08-29 10:40:37 +0200

    y

--- js/main.js
+++ js/main.js
@@ -68,3 +68,14 @@ window.addEventListener("keydown", function(e) {
     next(document.getElementsByClassName('top'))
   }
 })
+
+function throttle(fn, wait) {
+  var time = Date.now();
+  return function() {
+    if ((time + wait - Date.now()) < 0) {
+      fn();
+      time = Date.now();
+    }
+  }
+}
+
